82a18441c6c0537c1924b1b0e449c425e36c219d
Joshua Herrmann <joshua.herrmann1@web.de>
2022-08-21 21:32:44 +0200

    updated links

--- src/app/__projectlist/projectlistservice.service.ts
+++ src/app/__projectlist/projectlistservice.service.ts
@@ -5,30 +5,29 @@ import { Injectable } from '@angular/core';
 })
 export class ProjectlistserviceService {
   projects:Array<any> = [{'projectName' : 'Skarkie',
-  'description' : 'Sharkie is a small object oriented Jump & Run game.',
+  'description' : 'Sharkie is a small object oriented Jump & Run game based on Javescript and HTML Canvas.',
   'image' : 'assets/image/project_img/sharkie_mockup.png',
   'github_url' : 'https://github.com/JoshuaHerrmann/Sharkie',
-'web_app_url' : 'https://joshua-herrmann.developerakademie.net/DevAc_Pjk_15_Sharkie/index.html'},
-  {'projectName' : 'Pokedex',
-  'description' : 'Pokedex based on the open Pokeapi v2. ',
+'web_app_url' : 'http://sharkie.joshua-herrmann.de/'},
+  {'projectName' : 'Pokédex',
+  'description' : 'Pokédex based on the open Pokéapi v2. Build with REST API and Javascript. ',
   'image' : 'assets/image/project_img/pokemon_mockup.png',
     'github_url' : 'https://github.com/JoshuaHerrmann/Pokedex',
-    'web_app_url' : 'https://joshua-herrmann.developerakademie.net/DevAc_Pjk_14_Pokedex/index.html'
-  },
+    'web_app_url' : 'http://pokedex.joshua-herrmann.de/'},
   {'projectName' : 'Portfolio',
-  'description' : 'My own portfolio is a huge project aswell. This site is created with Angular.',
+  'description' : 'My Own Portfolio, completly build with Angular. I gained a lot experience by coding it and maintenance.',
   'image' : 'assets/image/project_img/portfolio_mockup.png',
   'github_url' : 'https://github.com/JoshuaHerrmann/Portfolio-J.H',
 'web_app_url' : '#'},
 {'projectName' : 'Slack Clone',
-'description' : 'A group project where we worked with Angular and Firebase.',
-'image' : 'assets/image/project_img/mockup_placeholder.png',
-'github_url' : 'https://github.com/JoshuaHerrmann',
-'web_app_url' : '#'},
-{'projectName' : 'Transport App',
+'description' : 'A group project where we worked with Angular and Firebase. Gained a lot experience coding in a team and debugging together.',
+'image' : 'assets/image/project_img/slackclone_mockup.png',
+'github_url' : 'https://github.com/AndreHehn/slackclone',
+'web_app_url' : 'http://slackclone.joshua-herrmann.de/main'},
+/* {'projectName' : 'Transport App',
   'description' : 'Transport app for hospital',
   'image' : 'assets/image/project_img/mockup_placeholder.png',
   'github_url' : 'https://github.com/Ephistotelis',
-'web_app_url' : '#'}]
+'web_app_url' : '#'} */]
   constructor() { }
 }
